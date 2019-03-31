e14ea04eb763a64f8d94f168a9d4fa180d492c68
Logan Shahan <loganshahan88@gmail.com>
2019-03-31 15:53:28 -0400

    started table

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,22 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS TeamGermany;
+CREATE DATABASE TeamGermany;
+USE TeamGermany;
+
+CREATE TABLE todos
+(
+	noteID int NOT NULL AUTO_INCREMENT,
+	title varchar(255) NOT NULL,
+    body varchar(255) NOT NULL,
+    category varchar(255) NOT NULL,
+	PRIMARY KEY (noteID),
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+
+
+
+
+
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
