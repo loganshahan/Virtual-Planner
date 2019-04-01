33839f1a011d186f209b94fbb4a2275757c6e8b6
Julian Hoyos <jhoyos135@gmail.com>
2019-03-31 21:34:09 -0400

    clean schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,22 +1,4 @@
 DROP DATABASE IF EXISTS FourGer;
 CREATE DATABASE FourGer;
-USE FourGer;
-CREATE TABLE todos
-(
-	noteID int NOT NULL AUTO_INCREMENT,
-	title varchar(255) NOT NULL,
-    body varchar(255) NOT NULL,
-    category varchar(255) NOT NULL,
-	PRIMARY KEY (noteID),
-    FOREIGN KEY (id) REFERENCES User(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
 
-
-
-
-
-
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
 
