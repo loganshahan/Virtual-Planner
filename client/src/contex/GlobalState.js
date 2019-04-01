6d5b87ae2fc3ee80914030072b6831f178b79231
Julian Hoyos <jhoyos135@gmail.com>
2019-04-01 19:43:12 -0400

    working with context api, not finish yet

---
+++ client/src/contex/GlobalState.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import UserContext from './user-context';
+
+export class GlobalState extends Component {
+
+    state =  {
+        login: []
+    }
+
+    getUser = async () => {
+        const res = await axios.get('/api/current-user');
+        const data = res.data
+        this.setState({
+            login: data
+        });
+        console.log(this.state.login)
+    };
+
+  
+  render() {
+    return (
+     <UserContext.Provider
+        value={{
+            getUser: this.getUser,
+            login: this.state.login
+        }}
+     >
+        {this.props.children}
+     </UserContext.Provider>
+
+    )
+  }
+}
+
+export default GlobalState
