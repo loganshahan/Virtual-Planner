ac42d659255eb9707e4d78122cbbddecab1c5b5c
enfenry <enfenry@vt.edu>
2019-04-06 13:38:12 -0400

    api/events/recent route added, calendar.js adjusted

--- routes/calendarApiRoutes.js
+++ routes/calendarApiRoutes.js
@@ -2,46 +2,65 @@ let db = require("../models");
 
 module.exports = function (app) {
 
-    app.get("/api/events", function(req, res) {
-        console.log(req.body)
-          let query = {};
-          if (req.query.id) {
-            query.id = req.query.id;
-          }
-      
-          db.Calendr.findAll({
-            where: query,
-            include: [db.User]
-          }).then(function(dbCalendr) {
-            res.json(dbCalendr);
-          });
-        });
+  app.get("/api/events", function (req, res) {
+    console.log(req.body)
+    let query = {};
+    if (req.query.id) {
+      query.id = req.query.id;
+    }
 
-    app.post("/api/events", function(req, res) {
-        db.Calendr.create(req.body).then(function(dbCalendr) {
-            res.json(dbCalendr);
-          });
+    db.Calendr.findAll({
+      where: query,
+      include: [db.User]
+    }).then(function (dbCalendr) {
+      res.json(dbCalendr);
     });
+  });
 
-    app.delete("/api/events/single/:event", function(req, res) {
-      db.Calendr.destroy({
-        where: {
-          id: req.params.event
-        }
-      }).then(function(dbCalendr) {
-        res.json(dbCalendr);
-      });
+  app.get("/api/events/recent", function (req, res) {
+    console.log(req.body)
+    let query = {};
+    if (req.query.id) {
+      query.id = req.query.id;
+      query.startDate = {[Op.gte]: req.query.startDate};
+    }
+
+    db.Calendr.findAll({
+      where: query,
+      order: [['start', 'ASC']],
+      limit: 3,
+      include: [db.User]
+    }).then(function (dbCalendr) {
+      res.json(dbCalendr);
     });
-    // DELETE all Events by UserId
-    app.delete("/api/events/:id", function(req, res) {
-      db.Calendr.destroy({
-        where: {
-          UserId: req.params.id
-        }
-      }).then(function(dbCalendr) {
-        res.json(dbCalendr);
-      });
+  });
+
+  app.post("/api/events", function (req, res) {
+    db.Calendr.create(req.body).then(function (dbCalendr) {
+      res.json(dbCalendr);
     });
-  
+  });
+
+  // DELETE a single Event by id
+  app.delete("/api/events/single/:eventid", function (req, res) {
+    db.Calendr.destroy({
+      where: {
+        id: req.params.eventid
+      }
+    }).then(function (dbCalendr) {
+      res.json(dbCalendr);
+    });
+  });
+  // DELETE all Events by UserId
+  app.delete("/api/events/:id", function (req, res) {
+    db.Calendr.destroy({
+      where: {
+        UserId: req.params.id
+      }
+    }).then(function (dbCalendr) {
+      res.json(dbCalendr);
+    });
+  });
+
 
 }
