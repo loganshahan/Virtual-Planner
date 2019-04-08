27763c253d913f6b3529dfbf782dea8908260d6b
Julian Hoyos <jhoyos135@gmail.com>
2019-04-08 05:18:20 -0400

    changed budget styles, added created date, and order by

---
+++ client/src/components/Dashboard/MainDashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class MainDashboard extends Component {
+
+    
+
+  render() {
+    return (
+      <div className="mt-4">
+            <h1>Dashboard</h1>
+      </div>
+    )
+  }
+}
+
+export default MainDashboard
