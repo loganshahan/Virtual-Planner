9f7c674a15e7f56cd4ccf2ae72ad036958872c6e
ansarkhan <ansarkhan@me.com>
2019-04-08 20:15:25 -0400

    homepage edits

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,17 +3,62 @@ import React from 'react';
 const  Welcome = () => {
   
     return (
-      <div className="full-height">
-        <div className="container custom_guess">
-            <div className="jumbotron">
-                <h1 className="display-2">Virtual Planner</h1>
-                <p className="lead">VP lets you work with your colleagues and get shit done</p>
-                <hr className="my-4" />
-                <p>Make lists. Add to-dos. Categorize your lists. All for free.</p>
-                <a className="btn btn-primary btn-lg" href="/auth/google" role="button">Sign Up Today!</a>
-            </div>
+      <div>
+        <div className="full-height">
+          <div className="container custom_guess">
+              <div className="jumbotron">
+                  <h1 className="display-2">Virtual Planner</h1>
+                  <p className="lead">VP is the best free virtual assistant</p>
+                  <hr className="my-4" />
+                  <p>Make to do lists. Track your budget. Manage your calendar. All for free.</p>
+                  <a className="btn btn-primary btn-lg" href="/auth/google" role="button">Sign Up Today!</a>
+              </div>
+          </div>
         </div>
+
+        <div className="row justify-content-center features">
+
+          <div className="col-4">
+            <div className="thumbnail">
+              <div className="caption">
+              <div className="custom_guess">
+                <i class="fas fa-list-ul fa-5x"></i>
+                <h3>Track your to dos</h3>
+              </div>
+                <p className="features">Add to dos to different cateogires. Track your progress by marking tasks complete. Categorize tasks into different lists.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-4">
+            <div className="thumbnail">
+              <div className="caption">
+              <div className="custom_guess">
+                <i class="fas fa-piggy-bank fa-5x"></i>
+                <h3>Manage your budget</h3>
+              </div>
+                <p className="features"> Track your income and expenses. Make sure you stay within budget! Get a snapshot using the dashboard.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="thumbnail">
+              <div className="caption">
+                <div className="custom_guess">
+                <i class="far fa-calendar-alt fa-5x"></i>
+                <h3>Update your calendar</h3>
+                </div>
+                <p className="features">Add events to your calendar to keep track of your schedule. View daily, weekly or monthly snapshots. </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        
       </div>
+
     )
 };
 
